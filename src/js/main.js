ea128a53c1ab4c34c5f89292c782b48c013898fd
sek1ra <sek1ra@yandex.ru>
2024-03-06 21:41:27 -0500

    add profile page, left to do prices table

--- src/js/main.js
+++ src/js/main.js
@@ -58,3 +58,82 @@ if( document.getElementById('portfolio-gallery') ) {
     mainGallery.mount();
     thumbGallery.mount();
 }
+
+const dropAreas = document.querySelectorAll('.drop-area');
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropAreas.forEach(dropArea => {
+    dropArea.addEventListener(eventName, preventDefaults, false);
+  });
+});
+
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+['dragenter', 'dragover'].forEach(eventName => {
+  dropAreas.forEach(dropArea => {
+    dropArea.addEventListener(eventName, highlight, false);
+  });
+  
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+  dropAreas.forEach(dropArea => {
+    dropArea.addEventListener(eventName, unhighlight, false);
+  });
+});
+
+function highlight(e) {
+  e.target.classList.add('highlight');
+}
+
+function unhighlight(e) {
+  e.target.classList.remove('highlight');
+}
+
+dropAreas.forEach(dropArea => {
+  dropArea.addEventListener('drop', function(e) {
+    handleDrop(e, this);
+  }, false);
+});
+
+function handleDrop(e, dropArea) {
+  const dt = e.dataTransfer;
+  const files = dt.files;
+  handleFiles(files, dropArea);
+}
+
+function handleFiles(files, dropArea) {
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (file.type.match('image.*')) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        const img = new Image();
+        img.src = e.target.result;
+        const imagePreview = dropArea.querySelector('.js-image-preview')
+        if(imagePreview) {
+            imagePreview.innerHTML = '';
+            imagePreview.appendChild(img);
+            dropArea.classList.add('loaded');
+        } else if(dropArea.dataset.imagepreview) {
+            const extImagePreview = document.getElementById(dropArea.dataset.imagepreview)
+            if(extImagePreview) {
+                extImagePreview.appendChild(img);
+            }
+        }
+      }
+      reader.readAsDataURL(file);
+    }
+  }
+}
+
+
+const filesElems = document.querySelectorAll('js-file-elem')
+filesElems.forEach( fileElem => {
+    fileElem.addEventListener('change', function(e) {
+        const files = this.files;
+        handleFiles(files, this);
+    });
+})
