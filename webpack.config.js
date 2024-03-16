36770312364d7ddd4f7a08fcf1b2327639eab9dd
sek1ra <sek1ra@yandex.ru>
2024-03-16 18:09:46 -0400

    Merge changes from product branch into development

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: ['./src/js/index.js','./src/css/style.scss'],
     output: {
         filename: 'bundle.js',
