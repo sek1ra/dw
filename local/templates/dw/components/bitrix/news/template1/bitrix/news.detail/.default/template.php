9b2bdfbe2815f38fa1b16486a5660b5c5043f46f
sek1ra <sek1ra@yandex.ru>
2024-04-03 20:42:47 -0400

    fix user filter

--- local/templates/dw/components/bitrix/news/template1/bitrix/news.detail/.default/template.php
+++ local/templates/dw/components/bitrix/news/template1/bitrix/news.detail/.default/template.php
@@ -100,10 +100,14 @@ $arUser = $rsUser->Fetch();
 			<?
 			global $USER;
 			if ($USER->IsAuthorized()) {
+				$currRating = 0;
+				if( $arResult['PROPERTIES']['RATING']['VALUE'] > 0 ) {
+					$currRating = $arResult['PROPERTIES']['RATING']['VALUE'];
+				}
 			?>
 			<div id="ratingWrapper" class="rating<?=( $arResult['PROPERTIES']['USERID']['VALUE'] == $USER->GetID() ? ' disabled' : '' )?>">
 				<div class="setRating">
-					<span class="curRating" style="width: <?=($arResult['PROPERTIES']['RATING']['VALUE']*100/5)?>%"></span>
+					<span class="curRating" style="width: <?=($currRating*100/5)?>%"></span>
 					<div class="stars">
 						<img
 							data-title="Плохо"
@@ -141,7 +145,7 @@ $arUser = $rsUser->Fetch();
 							data-index="5"
 							alt="">
     				</div>
-					<span class="count"><?=$arResult['PROPERTIES']['VOTES_COUNT']['VALUE']?></span>
+					<span class="count"><?=intval($arResult['PROPERTIES']['VOTES_COUNT']['VALUE'])?></span>
 				</div>
 				<input type="hidden" id="currentRating" value="<?=$arResult['PROPERTIES']['RATING']['VALUE']?>">
 				<input type="hidden" id="postid" value="<?=$arResult['ID']?>">
@@ -176,7 +180,7 @@ $arUser = $rsUser->Fetch();
 	<div class="colored pt75">
 		<section>
 			<?php
-			$userServicesFilter = array( 'IBLOCK_ID' => 7, 'ACTIVE' => 'Y', 'PROPERTY_USER_VALUE' => $USER->GetID() );
+			$userServicesFilter = array( 'IBLOCK_ID' => 7, 'ACTIVE' => 'Y', 'PROPERTY_USER' => $USER->GetID() );
 			$userServicesItems = CIBlockElement::GetList( 
 				array('SORT' => 'ASC'),
 				$userServicesFilter, 
@@ -243,7 +247,7 @@ $arUser = $rsUser->Fetch();
             $projectsFilter = array( 
                 'IBLOCK_ID' => 5, 
                 'ACTIVE' => 'Y',
-                'PROPERTY_USERID_VALUE' => $USER->GetID(),
+                'PROPERTY_USERID' => $USER->GetID(),
                 '!ID' => Array( $arResult['ID'] )
             );
             $projectsItems = CIBlockElement::GetList( 
@@ -294,85 +298,3 @@ $arUser = $rsUser->Fetch();
 		</section>
 	</div>
 </main>
-<?/*
-<div class="news-detail">
-	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-		<img
-			class="detail_picture"
-			border="0"
-			src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
-			width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
-			height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
-			alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
-			title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
-			/>
-	<?endif?>
-	<?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
-		<span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
-	<?endif;?>
-	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
-		<h3><?=$arResult["NAME"]?></h3>
-	<?endif;?>
-	<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && ($arResult["FIELDS"]["PREVIEW_TEXT"] ?? '')):?>
-		<p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
-	<?endif;?>
-	<?if($arResult["NAV_RESULT"]):?>
-		<?if($arParams["DISPLAY_TOP_PAGER"]):?><?=$arResult["NAV_STRING"]?><br /><?endif;?>
-		<?echo $arResult["NAV_TEXT"];?>
-		<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?><br /><?=$arResult["NAV_STRING"]?><?endif;?>
-	<?elseif($arResult["DETAIL_TEXT"] <> ''):?>
-		<?echo $arResult["DETAIL_TEXT"];?>
-	<?else:?>
-		<?echo $arResult["PREVIEW_TEXT"];?>
-	<?endif?>
-	<div style="clear:both"></div>
-	<br />
-	<?foreach($arResult["FIELDS"] as $code=>$value):
-		if ('PREVIEW_PICTURE' == $code || 'DETAIL_PICTURE' == $code)
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?
-			if (!empty($value) && is_array($value))
-			{
-				?><img border="0" src="<?=$value["SRC"]?>" width="<?=$value["WIDTH"]?>" height="<?=$value["HEIGHT"]?>"><?
-			}
-		}
-		else
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?><?
-		}
-		?><br />
-	<?endforeach;
-	foreach($arResult["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-
-		<?=$arProperty["NAME"]?>:&nbsp;
-		<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-			<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-		<?else:?>
-			<?=$arProperty["DISPLAY_VALUE"];?>
-		<?endif?>
-		<br />
-	<?endforeach;
-	if(array_key_exists("USE_SHARE", $arParams) && $arParams["USE_SHARE"] == "Y")
-	{
-		?>
-		<div class="news-detail-share">
-			<noindex>
-			<?
-			$APPLICATION->IncludeComponent("bitrix:main.share", "", array(
-					"HANDLERS" => $arParams["SHARE_HANDLERS"],
-					"PAGE_URL" => $arResult["~DETAIL_PAGE_URL"],
-					"PAGE_TITLE" => $arResult["~NAME"],
-					"SHORTEN_URL_LOGIN" => $arParams["SHARE_SHORTEN_URL_LOGIN"],
-					"SHORTEN_URL_KEY" => $arParams["SHARE_SHORTEN_URL_KEY"],
-					"HIDE" => $arParams["SHARE_HIDE"],
-				),
-				$component,
-				array("HIDE_ICONS" => "Y")
-			);
-			?>
-			</noindex>
-		</div>
-		<?
-	}
-	?>
-</div>*/?>
